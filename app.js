992847793de3c9d2ba115d874eb9f653761eec72
webdesignerADR <156018301+webdesignerADR@users.noreply.github.com>
2024-01-08 13:56:31 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider1').bxSlider({
+        mode: 'fade'
+    })
+    $('.slider2').bxSlider({
+        mode: 'fade'
+    })
+    $('.slider3').bxSlider({
+        mode: 'fade'
+    })
+})
